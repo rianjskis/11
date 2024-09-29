5e7b7de1497225cc39070f39937ac41b68ecd713
rianjskis <86937445+rianjskis@users.noreply.github.com>
2024-09-29 21:56:16 +0800

    Update script.py

--- script.py
+++ script.py
@@ -1,33 +1,33 @@
-import requests
+import json
 
-def fetch_and_convert_github_hosts(github_raw_url):
-    # Fetch the file content from GitHub
-    response = requests.get(github_raw_url)
-    if response.status_code != 200:
-        raise Exception(f"Failed to fetch file from GitHub: {response.status_code}")
+def format_domain(List):
+    domain = []
+    for line in List:
+        domain_lines = f"{line.strip()}"
+        domain.append(domain_lines)
+    return domain
 
-    all_domain = []
-    hosts_data = response.text.splitlines()  # Read file line by line
 
-    for line in hosts_data:
-        # Skip comments and empty lines
-        if line.startswith('#') or not line.strip() or '127.0.0.1' in line:
-            continue
-        
-        parts = line.split()
-        if len(parts) >= 2:  # Only take the first two columns, ignore extra columns
-            ip, domain = parts[:2]
-            all_domain.append(domain)
+def format_regex(List):
+    regex = []
+    for line in List:
+        regex_lines = f"{line.strip()}"
+        regex.append(regex_lines)
+    return regex
 
-    return all_domain  # Return the list of domains
-
-# Example GitHub hosts file link
-github_raw_url = "https://raw.githubusercontent.com/lingeringsound/10007_auto/master/reward"
-
-# Fetch and convert the hosts file
-all_domains = fetch_and_convert_github_hosts(github_raw_url)
-
-# Write the results to a .srs file
-with open('singbox_rules.srs', 'w') as outfile:
-    for domain in all_domains:
-        outfile.write(f"{domain}\n")  # Directly write the domain
+def build(rule):
+    domain_list = format_domain(rule.domain_list)
+    regex_list = format_regex(rule.regex_list)
+    List = {
+        "version": 1,
+        "rules": [
+            {
+                "domain": domain_list,
+                "domain_regex": regex_list,
+                "domain_suffix": []
+            }
+        ]
+    }
+    
+    json_data = [json.dumps(List, indent=2)]
+    return {'list': json_data, 'suffix': '.json', 'comment': '//', 'total': len(json_data)}
